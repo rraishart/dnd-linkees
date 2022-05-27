c0c054cd3faf2ad18063985e9118339a8abb3d7b
Ryan Raishart <rraishart@gmail.com>
2022-05-27 06:38:51 -0400

    New Cards

--- src/assets/items.js
+++ src/assets/items.js
@@ -6,55 +6,66 @@ import dribbble from '../assets/dribbble.png'
 import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
 import twitter from '../assets/twitter.png'
+import spotify from '../assets/spotify.png'
+import tiktok from '../assets/tiktok.png'
+import patreon from '../assets/patreon.png'
 
-const items = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
-},
-{
-    "title": "GitHub",
-    "subtitle": "@heysagnik | 🏡 to all my open-source projects",
-    "image": github,
-    "link": "https://github.com/heysagnik" //Github Profile link
-},
-{
-    "title": "Instagram",
-    "subtitle": "@heysagnik |  Shots of my life 🙂",
-    "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
-},
+
+const items = [
+    
+//     {
+//     "title": "Website",
+//     "subtitle": "Look at my work!",
+//     "image": website,
+//     "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
+// },
+// {
+//     "title": "GitHub",
+//     "subtitle": "@heysagnik | 🏡 to all my open-source projects",
+//     "image": github,
+//     "link": "https://github.com/heysagnik" //Github Profile link
+// },
+// {
+//     "title": "Instagram",
+//     "subtitle": "@heysagnik |  Shots of my life 🙂",
+//     "image": instagram,
+//     "link": "https://instagram.com/heysagnik" //instagram profile link 
+// },
 {
     "title": "Twitter",
-    "subtitle": "@heysagnik | 😉",
+    "subtitle": "@hDnDimebags | 😉",
     "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
-},
-{
-    "title": "LinkedIn",
-    "subtitle": "Know more about my profession 👨‍💼",
-    "image": linkedin,
-    "link": "https://www.linkedin.com/in/heysagnik/" // linkedin
-},
-{
-    "title": "YouTube",
-    "subtitle": "CODER SAGNIK | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
+    "link": "https://twitter.com/DnDimebags"// twitter profile link 
 },
 
 {
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | shots of dezigns ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+    "title": "TikTok",
+    "subtitle": "@DnDimebags | We may or may not post here",
+    "image": tiktok,
+    "link": "https://www.tiktok.com/@DnDimebags" // tiktok
 },
+
 {
-    "title": "Telegram",
-    "subtitle": "@heysagnik | Chat with me instantly ",
-    "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
-}]
+    "title": "YouTube",
+    "subtitle": " Official YouTube channel ",
+    "image": youtube,
+    "link": "https://www.youtube.com/"//youtube channel link 
+}
+,
+
+{
+    "title": "Spotify",
+    "subtitle": "Listen to our latest podcast",
+    "image": spotify,
+    "link": "https://open.spotify.com" // Spotify profile link 
+}
+,
+{
+    "title": "Patreon",
+    "subtitle": "The best way to support us",
+    "image": patreon,
+    "link": "https://patreon.com/" // Our Patreon
+}
+]
 
 export default items
