fd204e375c3797d89e4b93575608d82f2b40671b
Ryan Raishart <rraishart@gmail.com>
2022-07-03 23:18:03 -0400

    links

--- src/assets/items.js
+++ src/assets/items.js
@@ -14,48 +14,56 @@ import apple from '../assets/apple.png'
 
 
 const items = [
-{
-    "title": "Twitter",
-    "subtitle": "@DnDimebags",
-    "image": twitter,
-    "link": "https://twitter.com/DnDimebags"// twitter profile link 
-},
-
-{
-    "title": "Apple Podcasts",
-    "subtitle": "Listen to our latest podcast",
-    "image": apple,
-    "link": "https://podcasts.apple.com/us/podcast/dungeons-dimebags/id1631747844"// subreddit link 
-},
-
-{
-    "title": "TikTok",
-    "subtitle": "@DnDimebags | We may or may not post here",
-    "image": tiktok,
-    "link": "https://www.tiktok.com/@DnDimebags" // tiktok
-},
-
-{
-    "title": "YouTube",
-    "subtitle": " Official YouTube channel ",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UChXfUDluT9ssVRLRIuSXtKA"//youtube channel link 
-}
-,
+// {
+//     "title": "Twitter",
+//     "subtitle": "@DnDimebags",
+//     "image": twitter,
+//     "link": "https://twitter.com/DnDimebags"// twitter profile link 
+// },
 
 {
     "title": "Spotify",
     "subtitle": "Listen to our latest podcast",
     "image": spotify,
     "link": "https://open.spotify.com/show/4bimDY79p77vmh97URnM2n?si=194619b4b4bd417c" // Spotify profile link 
-}
-,
+},
+
 {
-    "title": "Patreon",
-    "subtitle": "The best way to support us",
-    "image": patreon,
-    "link": "https://www.patreon.com/dndimebags/" // Our Patreon
+    "title": "Apple Podcasts",
+    "subtitle": "Listen to our latest podcast",
+    "image": apple,
+    "link": "https://podcasts.apple.com/us/podcast/dungeons-dimebags/id1631747844"// apple link 
+},
+
+// {
+//     "title": "TikTok",
+//     "subtitle": "@DnDimebags | We may or may not post here",
+//     "image": tiktok,
+//     "link": "https://www.tiktok.com/@DnDimebags" // tiktok
+// },
+
+// {
+//     "title": "YouTube",
+//     "subtitle": " Official YouTube channel ",
+//     "image": youtube,
+//     "link": "https://www.youtube.com/channel/UChXfUDluT9ssVRLRIuSXtKA"//youtube channel link 
+// }
+// ,
+
+
+{
+    "title": "Buzzsprout",
+    "subtitle": "All other platforms",
+    "image": buzzsprout,
+    "link": "https://www.buzzsprout.com/2002608" // Buzzsprout profile link 
 }
+// ,
+// {
+//     "title": "Patreon",
+//     "subtitle": "The best way to support us",
+//     "image": patreon,
+//     "link": "https://www.patreon.com/dndimebags/" // Our Patreon
+// }
 ]
 
 export default items
